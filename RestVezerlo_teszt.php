0ebd72596ea3e73eac1f6b2f8ba4a8a4c07a6de1
bakknoel <bakknoel05@gmail.com>
2025-02-17 13:51:39 +0100

    RestVezerlo.php teszt előtt

---
+++ RestVezerlo_teszt.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once("RestVezerlo.php");
+
+$FishingRodrest = new $fishingRodRestKezelo();
+echo "Összes horgászbot lekérdezése: ";
+$_GET["view"] = "all";
+$FishingRodrest-> getAllFRod();
+echo "\n";
+
+$_GET["view"] = "single";
+$_GET["id"] = 1;
+$FishingRodrest->getFRodById($_GET["id"]);
+echo "\n";
+
+$_GET["view"] = "single";
+$_GET["id"] = 9999;
+$FishingRodrest->getFRodById($_GET["id"]);
+echo "\n";
+
+$_GET["view"] = "tipus";
+$_GET["tid"] = "Rakos";
+$FishingRodrest->getFRodByType($_GET["tid"]);
+echo "\n";
+
+$_GET["view"] = "tipus";
+$_GET["tid"] = "Unknown";
+$FishingRodrest->getFRodByType($_GET["tid"]);
+echo "\n";
+
+//Teszt hibás kérés
+$_GET["view"] = "invalid";
+$FishingRodrest->getFault();
+
+?>
